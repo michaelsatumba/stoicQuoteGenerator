7cb4414208af1e7aa15fa36b013143a93470a02b
michaelsatumba <mjsatumba@gmail.com>
2023-09-18 19:39:57 -0700

    reading list, share

--- script.js
+++ script.js
@@ -30,3 +30,24 @@ getStoicQuote();
 
 
 document.getElementById("newQuote").addEventListener("click", getStoicQuote);
+
+// Check if the Web Share API is available
+if (navigator.share) {
+  document.getElementById('shareQuote').addEventListener('click', async () => {
+    const text = document.getElementById('quoteText').innerText;
+    const author = document.getElementById('quoteAuthor').innerText;
+    const shareData = {
+      title: 'Stoic Quote of the Day',
+      text: `${text} — ${author}`,
+      url: window.location.href
+    };
+    try {
+      await navigator.share(shareData);
+    } catch(err) {
+      console.error('Sharing failed:', err);
+    }
+  });
+} else {
+  // Hide the share button if the Web Share API is not supported
+  document.getElementById('shareQuote').style.display = 'none';
+}
