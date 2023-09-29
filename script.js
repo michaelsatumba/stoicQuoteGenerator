9a5dc999ac4140329362fa5a1cd99bf52a352839
michaelsatumba <mjsatumba@gmail.com>
2023-09-29 08:02:51 -0700

    comments

--- script.js
+++ script.js
@@ -1,53 +1,65 @@
+// Check if service workers are supported in the current browser
 if ('serviceWorker' in navigator) {
-    navigator.serviceWorker.register('service-worker.js')
-    .then((registration) => {
+  // Register the service worker
+  navigator.serviceWorker.register('service-worker.js')
+  .then((registration) => {
+      // Log the successful registration of the service worker
       console.log('Service Worker Registered', registration);
-    })
-    .catch((error) => {
+  })
+  .catch((error) => {
+      // Log the error if the service worker registration fails
       console.log('Service Worker Registration Failed', error);
-    });
-  }
-
-  // Fetch quote from API
-async function getStoicQuote() {
-  try {
-    const response = await fetch('https://stoic-quotes.com/api/quote');
-    const data = await response.json();
-    const { text, author } = data;
-    // console.log("data", data) // debug
-
-    document.getElementById("quoteText").innerText = `"${text}"`;
-    document.getElementById("quoteAuthor").innerText = `- ${author}`;
-
-
-  } catch (error) {
-    console.error("There was a problem fetching the quote: ", error);
-  }
+  });
 }
 
+// Function to fetch a quote from the Stoic Quotes API
+async function getStoicQuote() {
+try {
+  // Fetch the quote
+  const response = await fetch('https://stoic-quotes.com/api/quote');
+  // Parse the response to JSON
+  const data = await response.json();
+  // Destructure the quote text and author from the data
+  const { text, author } = data;
 
+  // Set the quote text and author in the DOM
+  document.getElementById("quoteText").innerText = `"${text}"`;
+  document.getElementById("quoteAuthor").innerText = `- ${author}`;
+
+} catch (error) {
+  // Log any errors that occur during the fetch
+  console.error("There was a problem fetching the quote: ", error);
+}
+}
+
+// Fetch a quote when the script loads
 getStoicQuote();
 
-
+// Add an event listener to the "new quote" button to fetch a new quote when clicked
 document.getElementById("newQuote").addEventListener("click", getStoicQuote);
 
 // Check if the Web Share API is available
 if (navigator.share) {
-  document.getElementById('shareQuote').addEventListener('click', async () => {
-    const text = document.getElementById('quoteText').innerText;
-    const author = document.getElementById('quoteAuthor').innerText;
-    const shareData = {
-      title: 'Stoic Quote of the Day',
-      text: `${text} — ${author}`,
-      url: window.location.href
-    };
-    try {
-      await navigator.share(shareData);
-    } catch(err) {
-      console.error('Sharing failed:', err);
-    }
-  });
+// Add an event listener to the "share quote" button to share the quote when clicked
+document.getElementById('shareQuote').addEventListener('click', async () => {
+  // Get the current quote and author from the DOM
+  const text = document.getElementById('quoteText').innerText;
+  const author = document.getElementById('quoteAuthor').innerText;
+  // Create the share data
+  const shareData = {
+    title: 'Stoic Quote of the Day',
+    text: `${text} — ${author}`,
+    url: window.location.href
+  };
+  try {
+    // Attempt to share the quote
+    await navigator.share(shareData);
+  } catch(err) {
+    // Log any errors that occur during sharing
+    console.error('Sharing failed:', err);
+  }
+});
 } else {
-  // Hide the share button if the Web Share API is not supported
-  document.getElementById('shareQuote').style.display = 'none';
+// Hide the "share quote" button if the Web Share API is not supported
+document.getElementById('shareQuote').style.display = 'none';
 }
--- service-worker.js
+++ service-worker.js
@@ -1,21 +1,28 @@
+// This event triggers when the service worker is installed.
 self.addEventListener('install', (event) => {
-    event.waitUntil(
-      caches.open('my-cache').then((cache) => {
-        return cache.addAll([
-          '/',
-          '/index.html',
-          '/styles.css',
-          '/script.js',
-        ]);
-      })
-    );
-  });
-  
-  self.addEventListener('fetch', (event) => {
-    event.respondWith(
-      caches.match(event.request).then((response) => {
-        return response || fetch(event.request);
-      })
-    );
-  });
-  
+  // Wait for the following promise to resolve before installing the service worker.
+  event.waitUntil(
+    // Open or create a cache named 'my-cache'.
+    caches.open('my-cache').then((cache) => {
+      // Add multiple files to the cache.
+      return cache.addAll([
+        '/',
+        '/index.html',
+        '/styles.css',
+        '/script.js',
+      ]);
+    })
+  );
+});
+
+// This event triggers for every network request made by the app.
+self.addEventListener('fetch', (event) => {
+  // Respond to the network request.
+  event.respondWith(
+    // Check if the requested file is in the cache.
+    caches.match(event.request).then((response) => {
+      // Return cached file if found, otherwise fetch from the network.
+      return response || fetch(event.request);
+    })
+  );
+});
